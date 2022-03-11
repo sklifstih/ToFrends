c8b15621ff25981689adbecf52104cfbc239150e
sklif <sklif@LAPTOP-0OMPM8QV>
2022-03-11 21:41:15 +0300

    Добавьте файлы проекта.

---
+++ ToFrends/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ToFrends
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    interface IUser
+    {
+        string Name { get; set; }
+        string Adress { get; set; }
+        string Login { get; set; }
+        string Pass { get; set; }
+
+    }
+
+    interface IBio 
+    {
+        object City { get; set; }
+        object Country { get; set; }
+        string Request { get; set; }
+        object Ideas { get; set; }
+
+    }
+
+    interface IChat
+    {
+        object Messang { get; set; }
+        public object GetMessan();
+        public void SendMessan();
+        object Files { get; set; }
+              
+    }
+
+
+}
